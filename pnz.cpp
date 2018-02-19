d0c344af232e305aab86d2ad3f32389e78468869
SangaviPandian <36364726+SangaviPandian@users.noreply.github.com>
2018-02-19 08:43:40 -0800

    Create pnz.cpp

---
+++ pnz.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a;
+	cout<<"enter the value:"<<endl;
+	cin>>a;
+	if(a<0)
+	{
+		cout<<"negative";
+	}
+	else if(a>0)
+	{
+		cout<<"positive";
+	}
+	else if(a=0)
+	{
+		cout<<"equal to zero";
+	}
+	else
+	{
+		cout<<"others";
+	}
+	return 0;
+}
